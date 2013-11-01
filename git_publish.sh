9c2a400a268b4d507540f5d94bfc5e91bf7da68d
Brett Uglow <buglow@odecee.com.au>
2013-11-02 08:30:13 +1100

    Refining publishing process - site is now generated directly to /den or /web folder (no _site)

--- git_publish.sh
+++ git_publish.sh
@@ -5,13 +5,19 @@ git checkout master
 
 # Rename the /web directory to .web
 mv web .web
+mv node_modules .node_modules
 
 # Remove all non . files
-rm -rF
+rm -rF *
 
 # Copy everything from .web to current dir
+rsync -a .web ./
 
 # Remove .web 
+rm .web
+
+# Re-add node_modules
+mv .node_modules node_modules
 
 # Add everything
 git add .
